583283469c21e0c10db7afc62521a8dc5a103724
Kart8ik <shrkrthk200518@gmail.com>
2026-03-21 13:03:48 +0530

    added new projects

--- data/ProjectsData.jsx
+++ data/ProjectsData.jsx
@@ -1,23 +1,39 @@
 export const projects = [
     {
-        name: 'CalPal',
-        description: 'Collaborative Task Management App',
-        duration: 'Feb 2025 - Mar 2025',
-        stack: ['react','express', 'nodejs', 'mongodb','tailwind','shadcn'],
-        github: 'https://github.com/Kart8ik/CALPAL_2.0',
-        link: 'https://calpal-app.vercel.app/',
-        explanation:'A Website that helps you organize your tasks every day with a built-in calendar and also create groups to schedule and plan with multiple people, see when your group members are free to plan effectively, all while keeping things needed private.',
-        image: 'calpal.png'
-    },
-    {
         name: 'LeetTrack',
         description: 'LeetCode Analytics Platform',
         duration: 'Oct 2025 - Nov 2025',
-        stack: ['react', 'typescript', 'supabase', 'postgresql','tailwind','shadcn'],
+        stack: ['react', 'typescript', 'supabase', 'postgresql', 'tailwind', 'shadcn'],
         github: 'https://github.com/Kart8ik/LeetCode-Analytics-Platform',
         link: 'https://leettrackpes.vercel.app/',
-        explanation:'A Website that helps track your LeetCode progress with your friends in a leaderboard, with a personal dashboard to see your progress, clean UI and role-based access to allow admins to manage user data designed around use for a university',
-        image: 'leettrack.png'
+        explanation: 'A Website that helps track your LeetCode progress with your friends in a leaderboard, with a personal dashboard to see your progress, clean UI and role-based access to allow admins to manage user data designed around use for a university',
+    },
+    {
+        name: 'VoiceAI',
+        description: 'An Open Source Alternative to Vapi, Retell AI, and Bland AI.',
+        duration: 'Feb 2026 - Mar 2026',
+        stack: ['python', 'typescript', 'docker'],
+        github: 'https://github.com/Kart8ik/Voice-AI-Platform',
+        // link: ,
+        explanation: 'The complete, production-ready platform for building, deploying, and managing Voice AI Agents. Built on LiveKit(Real- time WebRTC) and Vobiz(Telephony / SIP).',
+    },
+    {
+        name: 'Portfolio-TUI',
+        description: 'A Portfolio in the terminal via npm',
+        duration: 'Feb 2026 - Feb 2026',
+        stack: ['react', 'nodejs'],
+        github: 'https://github.com/Kart8ik/portfolio-TUI',
+        link: 'https://www.npmjs.com/package/shrikarthik',
+        explanation: 'This is a clean terminal portfolio that runs as an npm CLI package. Built using React + ink, run the command `npx shrikarthik` to see the portfolio',
+    },
+    {
+        name: 'CalPal',
+        description: 'Collaborative Task Management App',
+        duration: 'Feb 2025 - Mar 2025',
+        stack: ['react', 'express', 'nodejs', 'mongodb', 'tailwind', 'shadcn'],
+        github: 'https://github.com/Kart8ik/CALPAL_2.0',
+        link: 'https://calpal-app.vercel.app/',
+        explanation: 'A Website that helps you organize your tasks every day with a built-in calendar and also create groups to schedule and plan with multiple people, see when your group members are free to plan effectively, all while keeping things needed private.',
     },
     {
         name: 'P2P-Echovoid',
@@ -26,17 +42,15 @@ export const projects = [
         stack: ['electron', 'javascript', 'nodejs'],
         github: 'https://github.com/Kart8ik/P2P_CHAT_APP',
         // link: ,
-        explanation:'A lightweight, peer-to-peer chat app built with Electron and custom networking libraries. No servers, no middlemen — just pure local network wizardry. Connect, broadcast, and chat with other agents on the same network.',
-        image: 'p2pechovoid.png'
+        explanation: 'A lightweight, peer-to-peer chat app built with Electron and custom networking libraries. No servers, no middlemen — just pure local network wizardry. Connect, broadcast, and chat with other agents on the same network.',
     },
     {
         name: 'Loopy',
         description: 'Create your own Background Music',
         duration: 'Jun 2025 - Ongoing',
-        stack: ['python', 'fastapi', 'react','typescript', 'tailwind','shadcn'],
+        stack: ['python', 'fastapi', 'react', 'typescript', 'tailwind', 'shadcn'],
         github: 'https://github.com/Kart8ik/LOOPY--audio-stem-transformer',
         // link: ,
-        explanation:'Tired of listening to generic background music when you are working? Wish you could listen to songs you like without vocals distracting you? Loopy-tune allows you to upload songs, removes vocals and then allows you to loop the background music to your liking',
-        image: 'loopy.png'
+        explanation: 'Tired of listening to generic background music when you are working? Wish you could listen to songs you like without vocals distracting you? Loopy-tune allows you to upload songs, removes vocals and then allows you to loop the background music to your liking',
     }
 ]
