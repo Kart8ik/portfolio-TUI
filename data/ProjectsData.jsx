a2f71b87ef88a5dd5f3b96378125bbd8d042ba3b
Kart8ik <shrkrthk200518@gmail.com>
2026-04-24 18:51:19 +0530

    added new projects

--- data/ProjectsData.jsx
+++ data/ProjectsData.jsx
@@ -1,12 +1,11 @@
 export const projects = [
     {
-        name: 'LeetTrack',
-        description: 'LeetCode Analytics Platform',
-        duration: 'Oct 2025 - Nov 2025',
-        stack: ['react', 'typescript', 'supabase', 'postgresql', 'tailwind', 'shadcn'],
-        github: 'https://github.com/Kart8ik/LeetCode-Analytics-Platform',
-        link: 'https://leettrackpes.vercel.app/',
-        explanation: 'A Website that helps track your LeetCode progress with your friends in a leaderboard, with a personal dashboard to see your progress, clean UI and role-based access to allow admins to manage user data designed around use for a university',
+        name: 'Code Forensics',
+        description: 'Study to evaluate if combining ML, static metrics, and LLMs improves code quality analysis.',
+        duration: 'Jan 2026 - Feb 2026',
+        stack: ['python', 'pandas', 'numpy', 'Llama 3'],
+        github: 'https://github.com/Kart8ik/code-forensics',
+        explanation: 'An empirical study evaluating whether combining static code metrics, classical machine learning, and LLM-based semantic reasoning provides better code quality analysis than using each technique independently.'
     },
     {
         name: 'VoiceAI',
@@ -15,34 +14,40 @@ export const projects = [
         stack: ['python', 'typescript', 'docker'],
         github: 'https://github.com/Kart8ik/Voice-AI-Platform',
         // link: ,
-        explanation: 'The complete, production-ready platform for building, deploying, and managing Voice AI Agents. Built on LiveKit(Real- time WebRTC) and Vobiz(Telephony / SIP).',
+        explanation: 'The complete, production-ready platform for building, deploying, and managing Voice AI Agents. Built on LiveKit(Real- time WebRTC) and Vobiz(Telephony / SIP).'
     },
     {
-        name: 'Portfolio-TUI',
-        description: 'A Portfolio in the terminal via npm',
-        duration: 'Feb 2026 - Feb 2026',
-        stack: ['react', 'nodejs'],
-        github: 'https://github.com/Kart8ik/portfolio-TUI',
-        link: 'https://www.npmjs.com/package/shrikarthik',
-        explanation: 'This is a clean terminal portfolio that runs as an npm CLI package. Built using React + ink, run the command `npx shrikarthik` to see the portfolio',
+        name: 'Mini-RAFT Paint 98',
+        description: 'A distributed collaborative drawing board inspired by MS Paint 98',
+        duration: 'Mar 2026 - Apr 2026',
+        stack: ['python', 'typescript', 'docker'],
+        github: 'https://github.com/Kart8ik/MS-Paint_98_Mini-Raft',
+        explanation: 'A real-time collaborative MS Paint 98 inspired drawing application powered by a distributed Mini-Raft consensus system to ensure consistent state across multiple nodes.'
     },
     {
-        name: 'CalPal',
-        description: 'Collaborative Task Management App',
-        duration: 'Feb 2025 - Mar 2025',
-        stack: ['react', 'express', 'nodejs', 'mongodb', 'tailwind', 'shadcn'],
-        github: 'https://github.com/Kart8ik/CALPAL_2.0',
-        link: 'https://calpal-app.vercel.app/',
-        explanation: 'A Website that helps you organize your tasks every day with a built-in calendar and also create groups to schedule and plan with multiple people, see when your group members are free to plan effectively, all while keeping things needed private.',
+        name: 'Self-Refuting RAG',
+        description: 'SR-RAG is a multi-agent QA system for accurate, transparent answers via claim verification.',
+        duration: 'Mar 2026 - Apr 2026',
+        stack: ['python', 'langchain', 'Llama 3', 'RAG', 'multi-agent'],
+        github: 'https://github.com/Kart8ik/Self-Refuting-RAG',
+        explanation: 'SR-RAG is a multi-agent question answering system designed to improve factual accuracy and transparency through claim-level adversarial verification.'
     },
     {
-        name: 'P2P-Echovoid',
-        description: 'Peer to Peer LAN Chat Application',
-        duration: 'Apr 2025 - Apr 2025',
-        stack: ['electron', 'javascript', 'nodejs'],
-        github: 'https://github.com/Kart8ik/P2P_CHAT_APP',
-        // link: ,
-        explanation: 'A lightweight, peer-to-peer chat app built with Electron and custom networking libraries. No servers, no middlemen — just pure local network wizardry. Connect, broadcast, and chat with other agents on the same network.',
+        name: 'FAIR-y',
+        description: 'Desktop app for annotating drawings and generating structured FAIR inspection reports.',
+        duration: 'Dec 2025 - Jan 2026',
+        stack: ['python'],
+        github: 'https://github.com/Kart8ik/FAIR-y_report-tool',
+        explanation: 'Desktop application for annotating engineering drawings with indexed inspection callouts, capturing dimensional data (characteristic, requirement, tolerances, equipment), and generating structured First Article Inspection Reports (FAIR) with project persistence and export workflows. Used by clients at two companies currently.'
+    },
+    {
+        name: 'LeetTrack',
+        description: 'LeetCode Analytics Platform',
+        duration: 'Oct 2025 - Nov 2025',
+        stack: ['react', 'typescript', 'supabase', 'postgresql', 'tailwind', 'shadcn'],
+        github: 'https://github.com/Kart8ik/LeetCode-Analytics-Platform',
+        link: 'https://leettrackpes.vercel.app/',
+        explanation: 'A Website that helps track your LeetCode progress with your friends in a leaderboard, with a personal dashboard to see your progress, clean UI and role-based access to allow admins to manage user data designed around use for a university'
     },
     {
         name: 'Loopy',
@@ -51,6 +56,33 @@ export const projects = [
         stack: ['python', 'fastapi', 'react', 'typescript', 'tailwind', 'shadcn'],
         github: 'https://github.com/Kart8ik/LOOPY--audio-stem-transformer',
         // link: ,
-        explanation: 'Tired of listening to generic background music when you are working? Wish you could listen to songs you like without vocals distracting you? Loopy-tune allows you to upload songs, removes vocals and then allows you to loop the background music to your liking',
+        explanation: 'Tired of listening to generic background music when you are working? Wish you could listen to songs you like without vocals distracting you? Loopy-tune allows you to upload songs, removes vocals and then allows you to loop the background music to your liking'
+    },
+    {
+        name: 'Portfolio-TUI',
+        description: 'A Portfolio in the terminal via npm',
+        duration: 'Feb 2026 - Feb 2026',
+        stack: ['react', 'nodejs'],
+        github: 'https://github.com/Kart8ik/portfolio-TUI',
+        link: 'https://www.npmjs.com/package/shrikarthik',
+        explanation: 'This is a clean terminal portfolio that runs as an npm CLI package. Built using React + ink, run the command `npx shrikarthik` to see the portfolio'
+    },
+    {
+        name: 'CalPal',
+        description: 'Collaborative Task Management App',
+        duration: 'Feb 2025 - Mar 2025',
+        stack: ['react', 'express', 'nodejs', 'mongodb', 'tailwind', 'shadcn'],
+        github: 'https://github.com/Kart8ik/CALPAL_2.0',
+        link: 'https://calpal-app.vercel.app/',
+        explanation: 'A Website that helps you organize your tasks every day with a built-in calendar and also create groups to schedule and plan with multiple people, see when your group members are free to plan effectively, all while keeping things needed private.'
+    },
+    {
+        name: 'P2P-Echovoid',
+        description: 'Peer to Peer LAN Chat Application',
+        duration: 'Apr 2025 - Apr 2025',
+        stack: ['electron', 'javascript', 'nodejs'],
+        github: 'https://github.com/Kart8ik/P2P_CHAT_APP',
+        // link: ,
+        explanation: 'A lightweight, peer-to-peer chat app built with Electron and custom networking libraries. No servers, no middlemen — just pure local network wizardry. Connect, broadcast, and chat with other agents on the same network.'
     }
 ]
